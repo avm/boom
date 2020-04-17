dad71e8e262a384f170bc6c54f3ce54adc4962a0
Alexander Myltsev <a.myltsev@gmail.com>
2020-04-17 12:15:43 +0300

    Win cards.

--- boomsite/boom/urls.py
+++ boomsite/boom/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('win', views.win_card, name='win_card'),
     path('<str:game_id>/add_cards', views.add_cards, name='add_cards'),
     path('<str:game_id>', views.game, name='game'),
     path('<str:game_id>/start', views.start_game, name='start_game'),
--- boomsite/boom/views.py
+++ boomsite/boom/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib import messages
-from .models import Game, Card
+from django.db.models import Max
+from .models import Game, Card, Score
 
 import random
+import json
 
 
 def index(request):
@@ -44,3 +46,25 @@ def start_game(request, game_id):
 def start_round(request, game_id, team_id):
 	game = get_object_or_404(Game, slug=game_id)
 	return render(request, 'boom/round.html', {'game': game, 'our_team': team_id})
+
+
+def win_card(request):
+	body = json.loads(request.body)
+	print('body:', body)
+	win_card = body.get('win_card')
+	if win_card is not None:
+		card = get_object_or_404(Card, pk=win_card)
+		team = body.get('our_team')
+		card.winning_team = team
+		score, created = Score.objects.get_or_create(game=card.game, team_id=team, defaults=dict(value=0))
+		score.value += 1
+		score.save()
+		card.save()
+	show_card = body.get('show_card')
+	if show_card:
+		# Move it into the back of the deck
+		card = get_object_or_404(Card, pk=show_card)
+		max_order = card.game.card_set.filter(winning_team=0).aggregate(Max('order'))['order__max']
+		card.order = max_order + 1
+		card.save()
+	return HttpResponse('ok')
